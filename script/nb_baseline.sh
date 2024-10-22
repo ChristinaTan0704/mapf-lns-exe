c243f45c261fe11db162bf0f5dc06a7a1c1e0ec4
ChristinaTan0704 <christinatan0704@gmail.com>
2024-10-22 09:18:20 -0700

    udpate rule base

--- script/nb_baseline.sh
+++ script/nb_baseline.sh
@@ -20,7 +20,7 @@ touch $cmd_file
 
 # Define arrays for all parameters
 agent_num=(500 350 350 600 900 750)
-map_name=("empty-32-32"  "den520d" ) # "random-32-32-20" "warehouse-10-20-10-2-1" "ost003d" "Paris_1_256"
+map_name=( "den520d" ) # "random-32-32-20" "warehouse-10-20-10-2-1" "ost003d" "Paris_1_256" "empty-32-32" 
 scene_num=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25)
 uniform_neighbor_neighborSize=("1 4" "0 4" "0 8" "0 16" "0 32" "3 4")
 
